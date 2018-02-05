e9a9bed1ec3d2427ef7e80d4ceec5f45dfe800a6
unknown <danielmedeiros52@gmail.com>
2018-02-05 15:30:56 -0300

    start futiba

---
+++ reactapp/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const pg = require('pg');
+const conString = "postgres://postgres:postgres@localhost/futiba";
+
+app.use(express.static('public'))
+app.set('view engine','ejs')
+app.get('/',(req , res)=>{
+    res.render('home')
+})
+
+
+pg.connect(conString, function(err, client, done) {
+
+  if (err) {
+    return console.error('Authentication failed ', err);
+  }
+
+
+  client.query('SELECT * FROM  pg_stat_activity', function(err, result) {
+      done();
+      
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows.number);
+  
+});
+pg.end()
+});
+
+app.listen(3000, err => {
+    console.log("Server on...")
+})
+
