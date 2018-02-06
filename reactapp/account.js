ea770f4c6968e98a043e7e12af50464e4c7de041
unknown <danielmedeiros52@gmail.com>
2018-02-06 15:02:56 -0300

    pg

---
+++ reactapp/account.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express.Router()
+
+app.get('/', async (req, res) => {
+
+
+
+    res.render('home')
+})
+
+
+module.exports = app
--- reactapp/index.js
+++ reactapp/index.js
@@ -1,37 +1,27 @@
 const express = require('express')
 const app = express()
-
-const pg = require('pg');
-const conString = "postgres://postgres:postgres@localhost/futiba";
-
-app.use(express.static('public'))
-app.set('view engine','ejs')
-app.get('/',(req , res)=>{
-    res.render('home')
-})
-
-
-pg.connect(conString, function(err, client, done) {
-
-  if (err) {
-    return console.error('Authentication failed ', err);
-  }
-
-
-  client.query('SELECT * FROM  pg_stat_activity', function(err, result) {
-      done();
-      
-    if (err) {
-      return console.error('error running query', err);
+const account = require('./account')
+const initOptions = {
+  error: (error, e) => {
+    if (e.cn) {
+      console.log('CN:', e.cn)
+      console.log('EVENT', error.message || error)
     }
-    console.log(result.console);
-  
-});
-pg.end()
-});
+  }
+};
+
+const pgp = require('pg-promise')(initOptions);
+const db = pgp('postgres://postgres:postgres@localhost/futiba')
+const init = async () => {
+  const connection = await db.connect()
+  console.log(connection)
+  app.use(account)
+}
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 app.listen(3000, err => {
-    console.log("Server on...")
+  console.log("Server on...")
 })
 
 //https://stackoverflow.com/questions/36120435/verify-database-connection-with-pg-promise-when-starting-an-app
